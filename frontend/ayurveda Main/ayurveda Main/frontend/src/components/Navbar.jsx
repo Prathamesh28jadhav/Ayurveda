dc76919a3ba2e84a0bebae8f5114c9524fb7e42b
Suraj Barate <baratesuraj8@gmail.com>
2025-04-05 21:25:00 +0530

    small changes is done in navbar

--- frontend/ayurveda Main/ayurveda Main/frontend/src/components/Navbar.jsx
+++ frontend/ayurveda Main/ayurveda Main/frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="bg-green-700 text-white p-4 flex justify-between items-center">
       <div className="text-xl font-bold">Ayurveda Bliss</div>
       <div>
-        <a href="/home" className="mx-2">Home</a>
+        <a href="/" className="mx-2">Home</a>
         <a href="/chatbot" className="mx-2">Chatbot</a>
         <a href="/login" className="mx-2">Login</a>
         <a href="/register" className="mx-2">Register</a>
